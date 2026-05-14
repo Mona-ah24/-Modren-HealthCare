b72efcdbdb34fbb24d90d41a5c9da22f886295c2
zainabmazroa-dotcom <zainabmazroa@gmail.com>
2026-05-14 18:04:10 +0300

    mona-1

--- lib/serverFirebase.dart
+++ lib/serverFirebase.dart
@@ -1,23 +1,23 @@
-// services/firebase_chat_service.dart
-import "package:cloud_firestore/cloud_firestore.dart";
-// import '../models/message.dart';
-import 'massage.dart';
+// // services/firebase_chat_service.dart
+// import "package:cloud_firestore/cloud_firestore.dart";
+// // import '../models/message.dart';
+// import 'massage.dart';
 
-class FirebaseChatService {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
+// class FirebaseChatService {
+//   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  Stream<List<Message>> getMessages(String chatRoomId) {
-    return _db.collection("chats/$chatRoomId/messages")
-        .orderBy("timestamp")
-        .snapshots()
-        .map((snapshot) =>
-        snapshot.docs.map((doc) => Message.fromMap(doc.data())).toList()
-    );
-  }
+//   Stream<List<Message>> getMessages(String chatRoomId) {
+//     return _db.collection("chats/$chatRoomId/messages")
+//         .orderBy("timestamp")
+//         .snapshots()
+//         .map((snapshot) =>
+//         snapshot.docs.map((doc) => Message.fromMap(doc.data())).toList()
+//     );
+//   }
 
-  Future<void> sendMessage(String chatRoomId, Message message) async {
-    await _db.collection("chats/$chatRoomId/messages")
-        .doc(message.messageId)
-        .set(message.toMap());
-  }
-}
+//   Future<void> sendMessage(String chatRoomId, Message message) async {
+//     await _db.collection("chats/$chatRoomId/messages")
+//         .doc(message.messageId)
+//         .set(message.toMap());
+//   }
+// }
